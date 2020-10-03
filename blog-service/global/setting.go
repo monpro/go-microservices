e77ae21977a9abea8dff561fc3687db47aaac21f
monpro <monproshen@gmail.com>
2020-10-03 21:46:26 +1000

    add config.yaml and setting.go to match the config file

---
+++ blog-service/global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/go-microservices/blog-service/pkg/settting"
+
+var (
+	ServerSetting   *settting.ServerSettingS
+	AppSetting      *settting.AppSettingS
+	DatabaseSetting *settting.DatabaseSettingS
+)
