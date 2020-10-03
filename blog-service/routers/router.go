3e2be399e8f03769267288fc2954133f662d80c6
monpro <monproshen@gmail.com>
2020-10-03 17:08:27 +1000

    add routers and service struct

---
+++ blog-service/routers/router.go
@@ -0,0 +1,29 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	v1 "github.com/go-microservices/blog-service/routers/api/v1"
+)
+func NewRouter() *gin.Engine {
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	article := v1.NewArticle()
+	tag := v1.NewTag()
+	apiV1 := router.Group("/api.v1")
+	{
+		apiV1.POST("/tags", tag.Create)
+		apiV1.DELETE("/tags/:id", tag.Delete)
+		apiV1.PUT("/tags/:id", tag.Update)
+		apiV1.PATCH("/tags/:id/state", tag.Update)
+		apiV1.GET("/tags", tag.List)
+
+		apiV1.POST("/articles", article.Create)
+		apiV1.DELETE("/articles/:id", article.Delete)
+		apiV1.PUT("/articles/:id", article.Update)
+		apiV1.PATCH("/articles/:id/state", article.Update)
+		apiV1.GET("/articles/:id", article.Get)
+		apiV1.GET("/articles", article.List)
+	}
+	return router
+}
