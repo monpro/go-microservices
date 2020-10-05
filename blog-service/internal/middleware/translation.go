a6b13210b3039523b1a16fdaae23cb21ad923889
monpro <monproshen@gmail.com>
2020-10-05 12:33:33 +1100

    add Translation middleware

--- blog-service/internal/middleware/translation.go
+++ blog-service/internal/middleware/translation.go
@@ -1,1 +1,36 @@
 package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	"github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
+	zh_translations "github.com/go-playground/validator/v10/translations/zh"
+)
+
+func Translation() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		uniLanguage := ut.New(en.New(), zh.New())
+		locale := context.GetHeader("locale")
+		translation, _ := uniLanguage.GetTranslator(locale)
+		value, status := binding.Validator.Engine().(*validator.Validate)
+		if status {
+			switch locale {
+			case "zh":
+				_ = zh_translations.RegisterDefaultTranslations(value, translation)
+				break
+			case "en":
+				_ = en_translations.RegisterDefaultTranslations(value, translation)
+				break
+			default:
+				_ = en_translations.RegisterDefaultTranslations(value, translation)
+				break
+			}
+			context.Set("trans", translation)
+		}
+		context.Next()
+	}
+}
--- blog-service/internal/routers/router.go
+++ blog-service/internal/routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-microservices/blog-service/docs"
+	"github.com/go-microservices/blog-service/internal/middleware"
 	v1 "github.com/go-microservices/blog-service/internal/routers/api/v1"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -11,6 +12,7 @@ func NewRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+	router.Use(middleware.Translation())
 	url := ginSwagger.URL("http://localhost:8000/swagger/doc.json")
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 	article := v1.NewArticle()
