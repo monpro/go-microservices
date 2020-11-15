db605530650bf772f4fad53933235b03fb2b458d
monpro <monproshen@gmail.com>
2020-11-15 16:23:35 +1100

    add dao of tag model

--- blog-service/internal/model/tag.go
+++ blog-service/internal/model/tag.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/go-microservices/blog-service/pkg/app"
+import (
+	"github.com/go-microservices/blog-service/pkg/app"
+	"github.com/jinzhu/gorm"
+)
 
 type Tag struct {
 	*Model
@@ -13,11 +16,69 @@ type TagSwapper struct {
 	Pager *app.Pager
 }
 
-type ArticleSwagger struct {
-	List []*Article
-	Pager *app.Pager
-}
-
 func (a Tag) TableName() string {
 	return "blog_tag"
+}
+
+func (t Tag) Create(db *gorm.DB) error  {
+	return db.Create(&t).Error
+}
+
+func (t Tag) Update(db *gorm.DB, values interface{}) error  {
+	return db.Model(&t).Where("id = ? and is_del = ?", t.ID, 0).Update(values).Error
+}
+
+func (t Tag) Delete(db *gorm.DB) error {
+	return db.Where("id = ? And is_del = ?", t.Model.ID, 0).Delete(&t).Error
+}
+
+func (t Tag) Get(db *gorm.DB) (Tag, error) {
+	var tag Tag
+	err := db.Where("id = ? AND is_del = ? AND state = ?", t.ID, 0, t.State).First(&tag).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return tag, err
+	}
+
+	return tag, nil
+}
+
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+	var err error
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	err = db.Where("is_del = ?", 0).Find(&tags).Error
+	if err != nil {
+		return nil, err
+	}
+	return tags, nil
+}
+
+func (t Tag) ListByIds(db *gorm.DB, ids []uint32)([]*Tag, error) {
+	var tags []*Tag
+	db = db.Where("state = ? And is_del = ?", t.State, 0)
+	err := db.Where("id In (?)", ids).Find(&tags).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return tags, nil
 }
