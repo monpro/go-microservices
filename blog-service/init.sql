2dd6258fd069cad4d443f27fa1c431e45febcba8
monpro <monproshen@gmail.com>
2020-12-08 21:46:19 +1100

    add create auth table sql

--- blog-service/init.sql
+++ blog-service/init.sql
@@ -2,6 +2,18 @@ create DATABASE
 IF NOT EXISTS blog_service DEFAULT CHARACTER
 SET utf8mb4 DEFAULT collate utf8mb4_general_ci;
 
+CREATE TABLE `blog_auth` (
+     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+     `app_key` varchar(20) DEFAULT '' COMMENT 'Key',
+     `app_secret` varchar(50) DEFAULT '' COMMENT 'Secret',
+     `created_on` int(10) unsigned DEFAULT '0',
+     `created_by` varchar(100) DEFAULT '',
+     `modified_on` int(10) unsigned DEFAULT '0',
+     `modified_by` varchar(100) DEFAULT '',
+     `deleted_on` int(10) unsigned DEFAULT '0',
+     `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '0 not deleted, 1 deleted',
+     PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='auth management';
 
 CREATE TABLE `blog_tag` (
     `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
